107403e3181ef2607914a92415a4fd3a6da108b6
John Wayne <ionut_popovici92@yahoo.com>
2017-05-02 00:14:25 +0300

    Python Basic

---
+++ if_elif_else.py
@@ -0,0 +1,10 @@
+name = "John"
+
+if name is "Adam":
+    print("Hey, Adam!")
+elif name is "Bob":
+    print("Hey, Bob!")
+elif name is "Chris":
+    print("Hey, Chris!")
+else:
+    print("Hey, John!!!")
