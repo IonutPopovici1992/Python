baea650656cfd1f3687e9ec232b0c29c1db17c14
John Doe <ionut_popovici92@yahoo.com>
2018-04-18 23:50:44 +0300

    Added: - keyword_arguments.py

---
+++ Python_3/keyword_arguments.py
@@ -0,0 +1,7 @@
+def write_sentence(name='Bucky', action='ate', item='tuna'):
+    print(name, action, item)
+
+write_sentence()
+write_sentence("Sally", "plays", "the guitar")
+write_sentence(action="is", item="awesome")
+write_sentence(name="Rob", action="is", item="awesome")
