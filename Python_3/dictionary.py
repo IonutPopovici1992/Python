c03042ae38fd59273a67a30e675f8772a842d135
John Doe <ionut_popovici92@yahoo.com>
2018-04-22 23:26:15 +0300

    Added: - sets.py; - dictionary.py

---
+++ Python_3/dictionary.py
@@ -0,0 +1,8 @@
+class_mates = {
+    'Tony': 'Cool but smells',
+    'Emma': 'Sits behind me',
+    'Lucy': 'Asks too many questions'
+}
+
+for key, value in class_mates.items():
+    print(key + " " + value)
---
+++ Python_3/sets.py
@@ -0,0 +1,7 @@
+groceries = {'cereal', 'milk', 'starcrunch', 'beer', 'duct tape', 'lotion', 'beer'}
+print(groceries)
+
+if 'milk' in groceries:
+    print("You already have milk!")
+else:
+    print("Uh yeah, you need milk!")
