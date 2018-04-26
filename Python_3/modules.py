f6d7f320006ece693d10af705c8dc723eb153e69
John Doe <ionut_popovici92@yahoo.com>
2018-04-26 23:52:33 +0300

    Added: - testModules.py; - modules.py

---
+++ Python_3/modules.py
@@ -0,0 +1,7 @@
+import testModules
+import random
+
+testModules.printMessage()
+
+x = random.randrange(1, 1000)
+print(x)
---
+++ Python_3/testModules.py
@@ -0,0 +1,2 @@
+def printMessage():
+    print("This is a function from testModules.py")
