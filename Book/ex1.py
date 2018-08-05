7709bdd53b272e8593fc901a49dac5ce8fefc1f1
Ionut Popovici <ionut_popovici92@yahoo.com>
2018-08-05 18:43:06 +0300

    Added Book folder: - ex1.py; - ex2.py; - ex3.py

---
+++ Book/ex1.py
@@ -0,0 +1,8 @@
+print("Hello, World!!!")
+print("Hello, Again!")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing...')
+# print("I'd much ruther you 'not'.")
+print('I "said" do not touch this.')
+print("I am John Wayne.")
