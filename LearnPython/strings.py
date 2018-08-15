9d12d6e880237215c9dc4487f4dec0adca80e3b0
John <ionut_popovici92@yahoo.com>
2018-08-15 23:36:25 +0300

    Added "LearnPython" folder: - hello_world.py; - print_test.py; - variables.py; - strings.py

---
+++ LearnPython/strings.py
@@ -0,0 +1,26 @@
+phrase = "Giraffe Academy"
+
+# len()
+print(len(phrase))
+
+print(phrase[0])
+print(phrase[8])
+
+# lower()
+print(phrase.lower())
+
+# upper()
+print(phrase.upper())
+
+# islower()
+print(phrase.lower().islower())
+
+# isupper()
+print(phrase.upper().isupper())
+
+# index()
+print(phrase.index("G"))
+print(phrase.index("Academy"))
+
+# replace()
+print(phrase.replace("Giraffe", "Elephant"))
