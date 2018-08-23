932b31d38e3112dee5cd5db90298137b42efa930
John <ionut_popovici92@yahoo.com>
2018-08-23 21:17:44 +0300

    Added: - numbers.py; - input.py; - basic_calculator.py

---
+++ LearnPython/basic_calculator.py
@@ -0,0 +1,6 @@
+number1 = input("Enter a number: ")
+number2 = input("Enter another number: ")
+
+result = float(number1) + float(number2)
+
+print(result)
---
+++ LearnPython/numbers.py
@@ -0,0 +1,41 @@
+from math import *
+# import math
+
+my_number = 5
+# str()
+print(str(my_number) + " is my favorite number.")
+
+my_number2 = -5
+# abs()
+print(abs(my_number2))
+
+# pow()
+print(pow(2, 10))
+print(pow(2, 100))
+
+# min()
+print(min(2, 10))
+
+# max()
+print(max(2, 10))
+
+# round()
+print(round(4.4))
+print(round(4.5))
+
+# floor()
+print(floor(4.4))
+print(floor(4.5))
+
+# ceil()
+print(ceil(4.4))
+print(ceil(4.5))
+
+# sqrt()
+print(sqrt(100))
+
+print(3 * 4 + 5)
+
+print(3 * (4 + 5))
+
+print(10 % 3)
