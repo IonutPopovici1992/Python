a9759b5bb6138c45f2cc51346fccc8cfbc600364
John <ionut_popovici92@yahoo.com>
2018-10-01 23:50:09 +0300

    Added lists.py

---
+++ LearnPython/lists.py
@@ -0,0 +1,29 @@
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Tim"]
+print(friends)
+
+print()
+
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[3])
+print(friends[4])
+
+print()
+
+print(friends[-1])
+print(friends[-2])
+print(friends[-3])
+print(friends[-4])
+print(friends[-5])
+
+print()
+
+print(friends[1:])
+print(friends[2:])
+print(friends[3:])
+
+print()
+
+friends[1] = "Mike"
+print(friends[1])
