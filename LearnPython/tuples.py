328fab4161dfaad8c290b22315e17ca401ebe29a
John <ionut_popovici92@yahoo.com>
2018-10-05 16:42:34 +0300

    Added tuples.py

---
+++ LearnPython/tuples.py
@@ -0,0 +1,8 @@
+coordinates = (5, 10)
+print(coordinates[0])
+print(coordinates[1])
+
+coordinates_list = [(5, 10), (15, 20), (25, 30)]
+print(coordinates_list[0])
+print(coordinates_list[1])
+print(coordinates_list[2])
