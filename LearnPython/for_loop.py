c91fbb01fcfa9085297acb07fae8b25e113ac7e7
John <ionut_popovici92@yahoo.com>
2018-10-07 02:17:29 +0300

    Added: - while_loop.py; - guessing_game.py; - for_loop.py

---
+++ LearnPython/for_loop.py
@@ -0,0 +1,17 @@
+friends = ["Jim", "Karen", "Kevin"]
+
+for name in friends:
+    print(name)
+
+print()
+
+for index in range(10):
+    print(index)
+
+print()
+
+for index2 in range(5):
+    if index2 == 0:
+        print("First iteration")
+    else:
+        print("Not first at all...")
