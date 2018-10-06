2547af747b6874c25277574a7214d0f6ea89a92f
John <ionut_popovici92@yahoo.com>
2018-10-06 22:16:44 +0300

    Added dictionaries.py

---
+++ LearnPython/dictionaries.py
@@ -0,0 +1,19 @@
+month_conversions = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
+}
+
+print(month_conversions[1])
+print(month_conversions.get(1))
+print(month_conversions.get(2))
+print(month_conversions.get(3))
