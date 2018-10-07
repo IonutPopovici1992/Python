c4a7bf7a02686ba3ccd3efa5f4f4f28a0f4b999a
John <ionut_popovici92@yahoo.com>
2018-10-07 21:21:56 +0300

    Added: - exponent_function.py; - 2D_lists.py; - translator.py; - comments.py; - try_except.py

---
+++ LearnPython/try_except.py
@@ -0,0 +1,8 @@
+try:
+    answer = 10 / 0
+    number = int(input("Enter a number: "))
+    print(number)
+except ZeroDivisionError as error:
+    print(error)
+except ValueError:
+    print("Invalid input")
