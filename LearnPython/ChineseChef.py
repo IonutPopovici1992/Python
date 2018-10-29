3f982253c747d7c6c31c87baefaaf538033ebd1e
John <ionut_popovici92@yahoo.com>
2018-10-30 00:08:29 +0200

    Inheritance

---
+++ LearnPython/ChineseChef.py
@@ -0,0 +1,6 @@
+from Chef import Chef
+
+class ChineseChef(Chef):
+
+    def make_fride_rice(self):
+        print("The chef makes fride rice.")
---
+++ LearnPython/inheritance.py
@@ -0,0 +1,13 @@
+from Chef import Chef
+from ChineseChef import ChineseChef
+
+myChef = Chef()
+myChef.make_special_dish()
+
+print()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_chicken()
+myChineseChef.make_salad()
+myChineseChef.make_special_dish()
+myChineseChef.make_fride_rice()
