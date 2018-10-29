6d1919e4c4bf8da4850baa4b532699546eac0e83
John <ionut_popovici92@yahoo.com>
2018-10-29 22:59:06 +0200

    Object Functions

--- LearnPython/classes_and_objects.py
+++ LearnPython/classes_and_objects.py
@@ -2,6 +2,11 @@ from student import Student
 
 student1 = Student("Jim", "Business", 3.1, False)
 student2 = Student("Pam", "Art", 2.5, True)
+student3 = Student("Michael", "Art", 3.8, True)
 
 print(student1.name)
 print(student2.major)
+
+print(student1.on_honor_roll())
+print(student2.on_honor_roll())
+print(student3.on_honor_roll())
