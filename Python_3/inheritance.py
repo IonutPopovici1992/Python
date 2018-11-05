80431af58fe2ee3047c34342b82e92e8b5e350b9
John <ionut_popovici92@yahoo.com>
2018-11-05 23:32:17 +0200

    Added: inheritance.py and multiple_inheritance.py

---
+++ Python_3/inheritance.py
@@ -0,0 +1,20 @@
+class Parent():
+
+    def print_last_name(self):
+        print('Roberts')
+
+
+class Child(Parent):
+
+    def print_first_name(self):
+        print('Bucky')
+
+    """
+    def print_last_name(self):
+        print('Roberts')
+    """
+
+
+bucky = Child()
+bucky.print_first_name()
+bucky.print_last_name()
