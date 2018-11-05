04b7578a6b703bb9091daeaaf6f381ded70e72a5
John <ionut_popovici92@yahoo.com>
2018-11-05 22:08:02 +0200

    Modified functions.py

--- Python_3/functions.py
+++ Python_3/functions.py
@@ -1,13 +1,13 @@
-def exampleFuncion():
+def exampleFunction():
     print("Yeah, functions are cool!")
 
 def bitcoin_to_usd(bitcoin):
     amount = bitcoin * 527
     print(amount)
 
-exampleFuncion()
-exampleFuncion()
-exampleFuncion()
+exampleFunction()
+exampleFunction()
+exampleFunction()
 
 bitcoin_to_usd(1)
 bitcoin_to_usd(5)
