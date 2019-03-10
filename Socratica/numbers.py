ac0b97868750a3e8be5d9a7b4775b6570486d2fe
John <ionut_popovici92@yahoo.com>
2019-03-10 22:50:35 +0200

    Added: hello_world.py, strings.py, numbers.py

---
+++ Socratica/numbers.py
@@ -0,0 +1,18 @@
+# Types of numbers: int, float, complex
+
+# int
+a = 496  # This is a perfect number
+print(type(a))
+print(a)
+
+# float
+e = 2.718281828
+print(type(e))
+print(e)
+
+# complex
+z = 2 - 6.1j
+print(type(z))
+print(z)
+print(z.real)
+print(z.imag)
---
+++ Socratica/strings.py
@@ -0,0 +1,16 @@
+# Strings
+
+message = "Meet me tonight."
+print(message)
+
+message2 = "The clock strikes at midnight."
+print(message2)
+
+message3 = 'I\'m looking for someone to share in an adventure.'
+message3 = "I'm looking for someone to share in an adventure."
+print(message3)
+
+movie_quote = """One of my favorite lines from The Godfather is:
+"I'm going to make him an offer he can't refuse."
+Do you know who said this?"""
+print(movie_quote)
