8934ab837c5e7e6e6bb6bac1402617ca85eec3dc
Ionut Popovici <ionut_popovici92@yahoo.com>
2019-09-30 18:18:24 +0300

    Socratica - Python: JSON in Python

---
+++ Socratica/JSON_1.py
@@ -0,0 +1,63 @@
+# JSON in Python
+
+# Key Methods
+# json.load(f): Load JSON data from file (or file-like object)
+# json.loads(s): Load JSON data from a string
+# json.dump(j, f): Write JSON object to file (or file-like object)
+# json.dumps(j): Output JSON object as string
+
+
+import json
+
+print(dir(json))
+
+print(50 * "-")
+
+json_file = open("D:/GitHub/Python/Socratica/movie_1.txt")
+movie = json.load(json_file)
+json_file.close()
+
+print(movie)
+
+print(50 * "-")
+
+print(type(movie))
+
+print(50 * "-")
+
+print(movie["title"])
+
+print(50 * "-")
+
+print(movie["release_year"])
+
+print(50 * "-")
+
+print(movie["actors"])
+
+print(50 * "-")
+
+value = """
+    {"title": "Tron: Legacy",
+     "composer": "Daft Punk",
+     "release_year": 2010,
+     "budget": 170000000,
+     "actors": null,
+     "won_oscar": false
+     }
+"""
+
+tron = json.loads(value)
+print(tron)
+
+print(50 * "-")
+
+print(movie)
+
+print(50 * "-")
+
+print(json.dumps(movie))
+
+print(50 * "-")
+
+print(json.dumps(movie, ensure_ascii=False))
---
+++ Socratica/JSON_2.py
@@ -0,0 +1,18 @@
+# JSON in Python
+
+
+import json
+
+movie2 = {}
+
+movie2["title"] = "Minority Report"
+movie2["director"] = "Steven Spielberg"
+movie2["composer"] = "John Williams"
+movie2["actors"] = ["Tom Cruise", "Collin Farrell", "Samantha Morton", "Max von Sydow"]
+movie2["is_awesome"] = True
+movie2["budget"] = 102000000
+movie2["cinematographer"] = "Janusz Kami\u0144ski"
+
+file2 = open("D:/GitHub/Python/Socratica/movie_2.txt", "w", encoding="utf-8")
+json.dump(movie2, file2, ensure_ascii=False)
+file2.close()
